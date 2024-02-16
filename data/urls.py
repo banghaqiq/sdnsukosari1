9cfb6b705f86d771a2e93ad4dd54ec545da14cb4
haqiq <ngabuburits018@gmail.com>
2024-02-16 15:18:03 +0700

    tambahan beberapa halaman

--- data/urls.py
+++ data/urls.py
@@ -8,15 +8,30 @@ urlpatterns = [
  path('profile/', views.profil, name='profile'),
  path('vm/', views.visimisi, name='visimisi'),
  path('developer/', views.developer, name='developer'),
+ path('struktur/', views.struktur, name='struktur'),
+ path('datasiswa/', views.datasiswa, name='datasiswa'),
 
 # Prestasi
+ path('kec/', views.kec, name='kecamatan'),
  path('kab/', views.kab, name='kabupaten'),
+ path('prov/', views.prov, name='provinsi'),
+ path('nas/', views.nas, name='nasional'),
 
 #  Performa
  path('classmeating/', views.classmeating, name='classmeating'),
+ path('drumband/', views.drumband, name='drumband'),
  path('studytour/', views.studytour, name='studytour'),
+ path('keagamaan/', views.keagamaan, name='keagamaan'),
+ path('penampilan/', views.penampilan, name='penampilan'),
+
+
+#  informasi
+ path('berita/', views.berita, name='berita'),
+ path('pengumuman/', views.pengumuman, name='pengumuman'),
+ path('extra/', views.extrakulikuler, name='extra'),
 
 
 #  path('testimonial/', views.alumni, name='testimonial'),
  path('contact/', views.contact, name='contact'),
+
 ]
--- data/views.py
+++ data/views.py
@@ -14,26 +14,58 @@ def visimisi(request):
 def developer(request):
  return render(request,'view/about/developer.html')
 
+def struktur(request):
+ return render(request,'view/about/struktur.html')
+
+def datasiswa(request):
+ return render(request,'view/about/datasiswa.html')
+
+
+
 
 
 # Prestasi
+def kec(request):
+ return render(request,'view/prestasi/kec.html')
 def kab(request):
  return render(request,'view/prestasi/kab.html')
+def prov(request):
+ return render(request,'view/prestasi/prov.html')
+def nas(request):
+ return render(request,'view/prestasi/nas.html')
+
+
 
 
 
 # performa
+def penampilan(request):
+ return render(request,'view/performa/penampilan.html')
+def drumband(request):
+ return render(request,'view/performa/drumband.html')
+def keagamaan(request):
+ return render(request,'view/performa/keagamaan.html')
 def classmeating(request):
  return render(request,'view/performa/classmeating.html')
-
 def studytour(request):
  return render(request,'view/performa/studytour.html')
+
+
+
 
 
 
 # alumni
 def alumni(request):
  return render(request,'view/testimonial.html')
+
+# informasi
+def berita(request):
+ return render(request, 'view/informasi/berita.html')
+def pengumuman(request):
+ return render(request, 'view/informasi/pengumuman.html')
+def extrakulikuler(request):
+ return render(request, 'view/informasi/extra.html')
 
 
 # contact
